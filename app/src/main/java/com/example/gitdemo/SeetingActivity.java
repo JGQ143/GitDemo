36c3eed74c68d78ace762f5233c083866d393284
JGQ143 <45384354+JGQ143@users.noreply.github.com>
2018-12-26 09:57:25 +0800

    第三次提交 初版提交2018年12月26日09:57:10 提交人:魏和尚

---
+++ app/src/main/java/com/example/gitdemo/SeetingActivity.java
@@ -0,0 +1,14 @@
+package com.example.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seeting);
+
+    }
+}
